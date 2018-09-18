2e09e2ce501e526b632bc51bf2e96a64b1f2c698
Kevin Chou <kchou723@gmail.com>
2018-09-18 17:31:50 -0400

    update babel plugin to use public class properties

--- src/components/example-class.js
+++ src/components/example-class.js
@@ -1,8 +1,22 @@
 import { Component } from 'react'
 
 class ExampleClass extends Component {
+  state = {
+    example: 'hello example'
+  }
+
+  getChild = () => {
+    return <div>this is child</div>
+  }
+
   render() {
-    return <div>This is example class component</div>
+    return (
+      <div>
+        <div>This is example class component</div>
+        { this.getChild() }
+        { this.state.example }
+      </div>
+    )
   }
 }
 
